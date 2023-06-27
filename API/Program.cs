29ba98553efe922a9116e73eea9ccc99b635ae86
Fahri Hanif <fahrihanif03@gmail.com>
2023-06-28 00:24:21 +0700

    Register services to container

--- API/Program.cs
+++ API/Program.cs
@@ -23,6 +23,13 @@ builder.Services.AddScoped<IRoleRepository, RoleRepository>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 
 // Register services
+builder.Services.AddScoped<AccountService>();
+builder.Services.AddScoped<AccountRoleService>();
+builder.Services.AddScoped<BookingService>();
+builder.Services.AddScoped<EducationService>();
+builder.Services.AddScoped<EmployeeService>();
+builder.Services.AddScoped<RoleService>();
+builder.Services.AddScoped<RoomService>();
 builder.Services.AddScoped<UniversityService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
